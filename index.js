9dc462a4c41f09bbdeb31c6f4fc0e0abd35a3b4a
Nick Kasten <kastencode@gmail.com>
2017-05-31 19:04:37 -0500

    hero data pulled from playoverwatch.com

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 const scrapeIt = require('scrape-it');
 
+
+// easy intro example
+
+/*
 scrapeIt("https://nocodewebscraping.com/top-10-web-scraping-books/", {
-    titles: "h3"
+  titles: "h3"
 }).then(page => {
-    const newList = page.titles.replace(/\d+./g, '\n');
-    console.log(newList);
+  const newList = page.titles.replace(/\d+./g, '\n');
+  console.log(newList);
 });
+*/
+
+// playoverwatch.com example
+
+scrapeIt("https://playoverwatch.com/en-us/career/pc/us/NILBOG-1950", {
+  qpHeroes: {
+    listItem: "#quickplay .progress-category.is-active>.progress-category-item",
+    data: {
+      name: {
+        selector: ".title"
+      },
+      playtime: {
+        selector: ".description"
+      }
+    }
+  },
+  compHeroes: {
+    listItem: "#competitive .progress-category.is-active>.progress-category-item",
+    data: {
+      name: {
+        selector: ".title"
+      },
+      playtime: {
+        selector: ".description"
+      }
+    }
+  }
+}).then(page => {
+  // const newList = page.titles.replace(/\d+./g, '\n');
+   console.log(page);
+
+});
+
+
